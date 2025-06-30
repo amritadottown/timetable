70f65c44e007f8a6ab6ecaf5f859fbc73dda5ab5
heftymouse <59918974+heftymouse@users.noreply.github.com>
2025-06-30 18:36:50 +0530

    shortname

--- app/src/main/java/me/heftymouse/timetable/models/Timetable.kt
+++ app/src/main/java/me/heftymouse/timetable/models/Timetable.kt
@@ -6,8 +6,9 @@ import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 
 @Serializable
 data class Timetable(
-    val subjects: HashMap<String, Subject>,
-    val schedule: HashMap<String, Array<String>>,
+  val subjects: HashMap<String, Subject>,
+  val schedule: HashMap<String, Array<String>>
+) {
     val slots: Array<String> = arrayOf(
         "8:10-9:00",
         "9:00-9:50",
@@ -15,15 +16,21 @@ data class Timetable(
         "11:00-11:50",
         "11:50-12:40",
         "2:00-2:50",
-        "2:50-3:40"),
+        "2:50-3:40"
+    )
     val labSlots: Array<String> = arrayOf(
         "8:10-10:25",
         "10:50-1:05",
         "1:25-3:40"
     )
-)
+}
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializable
 @JsonIgnoreUnknownKeys
-data class Subject(val name: String, val code: String, val faculty: String)
+data class Subject(
+  val name: String,
+  val code: String,
+  val faculty: String,
+  val shortName: String = name.split(" ").map({ e -> e[0] }).joinToString(separator = "")
+)
